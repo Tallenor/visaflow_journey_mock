4918982c481570100178ac2c9451625c83ac45a8
kaligraphy247 <chikeziejames3@gmail.com>
2024-12-09 18:34:45 +0100

    set up complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { ModeToggle } from "@/components/toggle-theme";
+
 
 export default function Home() {
   return (
       <main className="">
-          start here
+        <div className="top-2 right-2 fixed">
+          <ModeToggle />
+        </div>        
       </main>
   );
 }
